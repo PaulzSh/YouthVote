6acb44377c26c54f50e8948fb8b822b0b2b993d9
tanng <tanngxy@163.com>
2023-09-22 22:31:58 +0800

    去除key

--- src/main/java/com/votez/common/config/DynamoDBConfig.java
+++ src/main/java/com/votez/common/config/DynamoDBConfig.java
@@ -11,16 +11,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DynamoDBConfig {
 
-    String accessKey = "AKIAWQZPCAL44WWQR763";
-    String secretKey = "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";
-
-
     @Bean
     public DynamoDB getDynamoDB() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        // 配置DynamoDB客户端
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .withRegion("us-east-1") // 设置所需的AWS区域
                 .build();
         return new DynamoDB(client);
